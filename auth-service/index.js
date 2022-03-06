59477b678ee0a3ae3fcf7ee61e638f15e003aef6
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 10:28:51 +0800

    add auth route

--- auth-service/index.js
+++ auth-service/index.js
@@ -6,6 +6,8 @@ require("dotenv").config();
 
 app.use(express.json());
 
+app.use("/auth", require("./routes/auth.route"));
+
 app.listen(PORT, async () => {
   await mongoose.connect(process.env.DB_URI, {
     keepAlive: true,
