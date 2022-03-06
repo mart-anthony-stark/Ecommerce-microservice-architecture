e60a0fa85adeb5292af2a40d8051d9d6fc0db035
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 19:23:56 +0800

    add products route

--- product/server.js
+++ product/server.js
@@ -10,15 +10,14 @@ app.use(express.json());
 
 let channel, connection;
 
-app.use("/products", require("./routes/product.route"));
-
 async function connect() {
   const amqpServer = "amqp://localhost:5672";
   connection = amqp.connect(amqpServer);
   channel = await (await connection).createChannel();
   await channel.assertQueue("PRODUCT");
 }
-
 // connect();
+
+app.use("/products", require("./routes/product.route"));
 
 init(app);
