d05af68757c66cf83e2ece7d2de857a441f9e6b2
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 18:16:14 +0800

    remove logs

--- auth/controller/auth.controller.js
+++ auth/controller/auth.controller.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcryptjs");
 
 module.exports = {
   signup: async (req, res) => {
-    // console.log(req.body);
     try {
       const user = new User(req.body);
       const salt = await bcrypt.genSalt(10);
