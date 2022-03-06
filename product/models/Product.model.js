78b7c429781236284574190dc8d0801241810bb1
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 19:01:41 +0800

    add product model

---
+++ product/models/Product.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
