c273baca23830a3192b1a9ebc9f2f7349f413e21
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 19:24:03 +0800

    add product router

--- product/routes/product.route.js
+++ product/routes/product.route.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const productController = require("../controller/product.controller");
+const isAuth = require("../../isAuthenticated");
 
 router.get("/", productController.getAllProduct);
+router.post("/create", isAuth, productController.createProduct);
 
 module.exports = router;
