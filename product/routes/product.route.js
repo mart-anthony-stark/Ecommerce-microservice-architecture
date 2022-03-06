be748f958d3971892316f348500797c46c968395
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 19:02:31 +0800

    fixed product controller

--- product/routes/product.route.js
+++ product/routes/product.route.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const postsController = require("../controller/posts.controller");
+const productController = require("../controller/product.controller");
 
-router.get("/", postsController.getAllPosts);
+router.get("/", productController.getAllProduct);
 
 module.exports = router;
