a55773c90eb5b62ca46c86c905df23cf80df43a1
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 10:16:35 +0800

    add express app

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+require("dotenv").config();
 
-app.listen(PORT, ()=>[
-    console.log(`Started running on port ${PORT}`)
-])
+app.use(express.json());
+
+app.listen(PORT, async () => {
+  await mongoose.connect(process.env.DB_URI, {
+    keepAlive: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log(`Auth service started running on port ${PORT}`);
+});
