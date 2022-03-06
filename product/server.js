8269542c5931beb869dc38297dc9e2960d061c4f
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 19:12:43 +0800

    add rabbitmq connect method

--- product/server.js
+++ product/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const init = require("./utils/init");
+const amqp = require("amqplib");
 require("dotenv").config({});
 
 const app = express();
@@ -7,6 +8,17 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+let channel, connection;
+
 app.use("/products", require("./routes/product.route"));
+
+async function connect() {
+  const amqpServer = "amqp://localhost:5672";
+  connection = amqp.connect(amqpServer);
+  channel = await (await connection).createChannel();
+  await channel.assertQueue("PRODUCT");
+}
+
+// connect();
 
 init(app);
