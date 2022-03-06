39ccbcc1a8a97c3f14ce0313b3cf857f1d4d2924
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 18:32:21 +0800

    add role

--- auth/models/User.model.js
+++ auth/models/User.model.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 8 },
+    role: { type: String, default: "user", enum: ["user", "admin"] },
   },
   { timestamps: true }
 );
