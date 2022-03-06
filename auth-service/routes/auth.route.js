8740ba1daab15c0154ca3e2f773b029a92af15a9
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 18:32:54 +0800

    delete

--- auth-service/routes/auth.route.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const AuthController = require("../controllers/auth.controller");
-
-const auth = new AuthController();
-router.get("/register", auth.register);
-
-module.exports = router;
