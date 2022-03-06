b0abc58b05fbfb11aa82e3077c03fb07bb3d1ba7
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 19:45:54 +0800

    add catcher

--- product/routes/product.route.js
+++ product/routes/product.route.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const productController = require("../controller/product.controller");
+const catcher = require("../utils/catcher");
 const isAuth = require("../../isAuthenticated");
 
 router.get("/", productController.getAllProduct);
-router.post("/create", isAuth, productController.createProduct);
+router.post("/create", isAuth, catcher(productController.createProduct));
 
 module.exports = router;
