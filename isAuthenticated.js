832afbc8ce7990952f59a7ba35a1da7ebd4ceb95
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 19:23:49 +0800

    fixed isauth error handling

--- isAuthenticated.js
+++ isAuthenticated.js
@@ -1,11 +1,17 @@
 const jwt = require("jsonwebtoken");
 
-export async function isAuthenticated(req, res, next) {
-  const token = req.headers["authorization"].split(" ")[1];
-  jwt.verify(token, "SADFAD354", (err, decoded) => {
-    if (err) return res.json(err);
+const isAuthenticated = (req, res, next) => {
+  try {
+    const token = req.headers["authorization"].split(" ")[1];
+    jwt.verify(token, "SADFAD354", (err, decoded) => {
+      if (err) return res.json(err);
 
-    req.user = decoded;
-    next();
-  });
-}
+      req.user = decoded;
+      next();
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+module.exports = isAuthenticated;
