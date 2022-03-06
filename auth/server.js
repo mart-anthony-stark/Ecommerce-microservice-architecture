66d800cece57903cd3497dc64968dfa9a076044a
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 18:15:24 +0800

    remove post route

--- auth/server.js
+++ auth/server.js
@@ -8,6 +8,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/auth", require("./routes/auth.route"));
-app.use("/posts", require("./routes/posts.route"));
 
 init(app);
