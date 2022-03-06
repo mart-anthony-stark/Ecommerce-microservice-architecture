8276f70812a692017fc2ecc49f3230b920afee96
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-06 20:05:07 +0800

    remove mq connection

--- product/server.js
+++ product/server.js
@@ -8,16 +8,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-let channel, connection;
-
-async function connect() {
-  const amqpServer = "amqp://localhost:5672";
-  connection = amqp.connect(amqpServer);
-  channel = await (await connection).createChannel();
-  await channel.assertQueue("PRODUCT");
-}
-// connect();
-
 app.use("/products", require("./routes/product.route"));
 
 init(app);
