9cf18a1bb216a093d7e4497081eda8a443ebe204
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-07 14:35:25 +0800

    add consume order queue

--- order/utils/createConn.js
+++ order/utils/createConn.js
@@ -1,7 +1,22 @@
+const amqp = require("amqplib");
+const { createOrder } = require("../controller/order.controller");
+
+let channel, connection;
 async function connect() {
   const amqpServer = "amqp://localhost:5672";
   connection = amqp.connect(amqpServer);
-  module.exports.channel = await (await connection).createChannel();
+  channel = await (await connection).createChannel();
   await channel.assertQueue("PRODUCT");
 }
-connect();
+connect().then(() => {
+  channel.consume("ORDER", (data) => {
+    const { products, email } = JSON.parse(data.content);
+
+    console.log("Consuming ORDER queue");
+    const newOrder = createOrder(products, email);
+    channel.ack(data);
+    channel.sendToQueue("PRODUCT", Buffer.from(JSON.stringify({ newOrder })));
+  });
+});
+
+module.exports = channel;
