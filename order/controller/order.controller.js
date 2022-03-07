ea293b0c6c0080e4d20fa6031d7a99e739b0b3c4
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-07 14:35:15 +0800

    add create order controller

--- order/controller/order.controller.js
+++ order/controller/order.controller.js
@@ -1,8 +1,22 @@
 const Order = require("../models/order.model");
+const { channel } = require("../utils/createConn");
 
 module.exports = {
-  getAllorders: async (req, res) => {
+  getAllOrders: async (req, res) => {
     const orders = await Order.find();
     res.send(orders);
+  },
+  createOrder: async (products, userEmail) => {
+    const total = products.reduce((t, item) => {
+      return (t += item.price);
+    }, 0);
+
+    const newOrder = new Order({
+      products,
+      user: userEmail,
+      total_price: total,
+    });
+    await newOrder.save();
+    return newOrder;
   },
 };
