fbc09974e346b3ed7ac0d9bcc29b99a400061d80
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-07 13:47:41 +0800

    delete

--- order/routes/posts.route.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const postsController = require("../controller/posts.controller");
-
-router.get("/", postsController.getAllPosts);
-
-module.exports = router;
